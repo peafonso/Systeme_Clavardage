d87f07e2909e3749ce1e796f7fef54306d0f52f0
peafonso <pafonso@etud.insa-toulouse.fr>
2020-12-03 12:18:31 +0100

    ajout mineur

--- src/system/Message.java
+++ src/system/Message.java
@@ -4,11 +4,13 @@ public class Message {
 	private User sender;
 	private User receiver;
 	private String data;
+	private int time; //variable pour l'horodatage
 	
-	public Message(User from, User to, String msg) {
+	public Message(User from, User to, String msg, int horodatage) {
 		this.setSender(from);
 		this.setReceiver(to);
 		this.setData(msg);
+		this.setTime(horodatage);
 	}
 
 	public String getData() {
@@ -33,6 +35,14 @@ public class Message {
 
 	public void setReceiver(User receiver) {
 		this.receiver = receiver;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
 	}
 
 }
