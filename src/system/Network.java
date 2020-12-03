29094337b85205f75d31f72497681a1c1929ad51
soumare <soumare@etud.insa-toulouse.fr>
2020-12-03 10:20:21 +0100

    Développement broadcast

--- src/system/Network.java
+++ src/system/Network.java
@@ -1,16 +1,58 @@
 package system;
+import java.net.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Objects;
 
 public class Network {
 	
 	//première connexion envoi d'un broadcast aux numéros de ports (même num de port si 
+	    private static DatagramSocket socket = null;
+
+	    public static void main(String[] args) throws IOException {
+	    	System.out.println("Envoi Hello");
+	        broadcast("Hello", InetAddress.getByName("10.255.255.255"));
+	        for (InetAddress addressDest : listAllBroadcastAddresses()) {
+		        broadcast("Hello", addressDest);
+	        }
+	    }
+
+	    public static void broadcast(String broadcastMessage, InetAddress address) throws IOException {
+	        socket = new DatagramSocket();
+	        socket.setBroadcast(true);
+
+	        byte[] buffer = broadcastMessage.getBytes();
+
+	        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
+	        socket.send(packet);
+	        socket.close();
+	    }
+	    
+	    static ArrayList<InetAddress> listAllBroadcastAddresses() throws SocketException {
+	        ArrayList<InetAddress> broadcastList = new ArrayList<>();
+	        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+	        while (interfaces.hasMoreElements()) {
+	            NetworkInterface networkInterface = interfaces.nextElement();
+
+	            if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+	                continue;
+	            }
+
+	            networkInterface.getInterfaceAddresses().stream() 
+	              .map(a -> a.getBroadcast())
+	              .filter(Objects::nonNull)
+	              .forEach(broadcastList::add);
+	        }
+	        return broadcastList;
+	    }
+	    
+	    }
+	
 	
 	//connexion déjà faite et communication entre deux utilisateurs
-	public void Conversation (user1, user2) {
+	//public void Conversation (user1, user2) {
 		
-	}
+	//}
 
-	public static void main(String[] args) {
-		
-	}
 
-}
