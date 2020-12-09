4142799a582b9703f84559756fac6c2627b108e6
soumare <soumare@etud.insa-toulouse.fr>
2020-12-09 12:25:57 +0100

    ajustement user

--- src/system/Test.java
+++ src/system/Test.java
@@ -1,20 +1,21 @@
 package system;
 
+//classe ouvrir conversation a intégrer à client
 public class Test {
 
 	public static void main(String[] args) {
 		//Test de change pseudo
-		/*User coumbz =  new User("127.0.0.1", 1234, "coucou");
-		User pepe =  new User("127.0.0.1", 1234, "pp");
+		User coumbz =  new User("127.0.0.1", 1234, "coucou");
 		User yaya = new User("127.0.0.1", 1234, "pp");
-		Contacts.showContacts();
-		coumbz.Change_pseudo("pp");
-		coumbz.Change_pseudo("pp2");
-		Contacts.showContacts();*/
+		coumbz.init();
+		coumbz.showContacts();
+		coumbz.Change_Pseudo("pp");
+		
 		
 		//Test Broadcast
-		SocketServer sok = new SocketServer("127.0.0.1",4445);
-		sok.Listen();
+		//SocketServer sok = new SocketServer("127.0.0.1",4445);
+		//tant qu'on appelle pas envoyer message on attend de recevoir quelque chose
+		//sok.Listen();
 	}
 
 }
--- src/system/User.java
+++ src/system/User.java
@@ -1,6 +1,12 @@
 package system;
 import java.util.Random;
-import java.util.Iterator;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 //rajouter un mot de passe pour les connexions et protéger les pseudos
 enum State {CONNECTED, CONNECTING, DISCONNECTED };
@@ -11,33 +17,62 @@ public class User {
 	private String pseudo;
 	private State state;
 	private int port;
+	static ArrayList<User> contacts = new ArrayList<User>();
 
-
+	public User(String address) {
+		this.setIP(address);
+	}
 
 	public User(String address, int port, String pseudonym) {
 		
 		//address
 		this.setIP(address);
-		
 		//port
 		this.setPort(port);
-		
-		//Pseudonyme (on vérifie l'unicité dès la création du User)
-		while (!(Change_pseudo(pseudonym))) {
-			pseudonym=Pseudo_Random(6);
-		}
-		
+		this.pseudo=pseudonym;
+
 		//Etat de l'utilisateur (connecté car construction)
-		this.setState(State.CONNECTED);
-		
-		addContact();
+		this.setState(State.CONNECTED);	
 		
 	}
 	
+	public void init() {
+		//Pseudonyme (on vérifie l'unicité dès la création du User)
+		Change_Pseudo(pseudo);
+		
+		try {
+			addContact();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	// On ajoute le user dans la liste contacts dès qu'il s'instancie
-	public void addContact() {
-		//KIKÉLA
-		Contacts.contacts.add(this);
+	public void addContact() throws IOException {
+		DatagramSocket socketRec = new DatagramSocket(this.port);
+        DatagramSocket socketEnvoi = new DatagramSocket();
+
+		byte[] array = new byte[100000000];
+		DatagramPacket receivePacket = new DatagramPacket(array,array.length);
+		User usertoadd= new User(receivePacket.getAddress().getHostAddress());
+
+		  while(true) {
+			  socketRec.receive(receivePacket);
+			  String otherpseudo = new String( receivePacket.getData(), 0, receivePacket.getLength());
+			  if (otherpseudo.equals(this.pseudo)) {
+				  //on prévient le user qui nous as envoyé son pseudo que l'unicité 
+				  //n'est pas respecté étant donné qu'il vient de choisir notre pseudo
+				  byte[] buffer = ("already used").getBytes();
+			      DatagramPacket packet = new DatagramPacket(buffer, buffer.length,4445);
+			      socketEnvoi.send(packet);
+			  }
+			  else {
+				  //on envoie son adresse IP pour qu'il nous ajoute dans sa liste
+				  byte[] buffer = (this.iP).getBytes();
+			      DatagramPacket packet = new DatagramPacket(buffer, buffer.length,4445);
+			      socketEnvoi.send(packet);
+			  }
+			  contacts.add(usertoadd);
+		  }
 	}
 	 
 	
@@ -50,7 +85,36 @@ public class User {
 		this.setState(State.DISCONNECTED);
 	}
 	
-	public boolean Change_pseudo(String pseudonym) {
+	public void Change_Pseudo(String pseudonym) {
+		try {
+			Network.broadcast(this.pseudo, InetAddress.getByName("255.255.255.255"));
+			Scanner scanner= new Scanner(System.in);
+			DatagramSocket socketRec = new DatagramSocket(this.port);
+			byte[] array = new byte[100000000];
+			DatagramPacket receivePacket = new DatagramPacket(array,array.length);
+			socketRec.receive(receivePacket);
+            String response = new String( receivePacket.getData(), 0, receivePacket.getLength() );
+            User usertoadd= new User(receivePacket.getAddress().getHostAddress());
+            if (response.equals("already used")) {
+            	//on change le pseudo car quelqu'un le possède déjà
+            	String newpseudo= scanner.next();
+            	this.pseudo=newpseudo;
+            	Change_Pseudo(newpseudo);
+            }
+            else {
+            	//if(!(contacts.contains()))
+            		contacts.add(usertoadd);
+            }
+            socketRec.close();
+            scanner.close();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		 
+	}
+	
+	/*public boolean Change_pseudo(String pseudonym) {
 		
 		//itérateur afin de vérifier si quelqu'un a le même pseudo 
 		Iterator<User> iter = Contacts.contacts.iterator();
@@ -70,6 +134,14 @@ public class User {
 		}
 		
 		return stop;
+	}*/
+	
+	//Methode affichant tous les contact de la liste
+	public static void showContacts() {
+		System.out.println("Contacts list:");
+		for (User user : contacts) {
+			System.out.println(user.getPseudo());
+		}
 	}
 	
 	public String Pseudo_Random(int length) {
