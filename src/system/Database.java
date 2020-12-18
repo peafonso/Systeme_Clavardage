1562b18cba1d912c00a588747b05b98f1733e77d
peafonso <pafonso@etud.insa-toulouse.fr>
2020-12-18 15:23:42 +0100

    debut database

--- src/system/Database.java
+++ src/system/Database.java
@@ -1,18 +1,54 @@
 package system;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
-
-	    /**
-	     * Connect to a sample database
-	     *
-	     * @param fileName the database file name
-	     */
-	    public static void createNewDatabase(String fileName) {
+	
+	/**
+     * Create a new table in the test database
+     *
+     */
+    public static void createTables() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/tests.db";
+        
+        // SQL statement for creating a new table of contatcs
+        String sqlcontacts = "CREATE TABLE IF NOT EXISTS contacts (\n"
+                + "	user User PRIMARY KEY,\n"
+                + ");";
+        
+        String sqlconversation = "CREATE TABLE IF NOT EXISTS conversations (\n"
+        		+ "	usera User PRIMARY KEY,\n"
+        		+ " userb User NOT NULL,\n"
+        		+ ");";
+        
+        String sqlmessages = "CREATE TABLE IF NOT EXISTS messsages (\n"
+        		+ "	text Message PRIMARY KEY,\n"
+        		+ ");";
+        
+                
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create new tables
+            stmt.execute(sqlcontacts);
+            stmt.execute(sqlconversation);
+            stmt.execute(sqlmessages);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+	/**
+	* Connect to a sample database
+	*
+	* @param fileName the database file name
+	*/
+	 public static void createNewDatabase(String fileName) {
 	    	try {
 				Class.forName("org.sqlite.JDBC");
 			} catch (ClassNotFoundException e1) {
@@ -31,13 +67,53 @@ public class Database {
 	        } catch (SQLException e) {
 	            System.out.println(e.getMessage());
 	        }
+	 }
+	 
+	 
+	   /**
+	     * Connect to the test.db database
+	     * @return the Connection object
+	     */
+	 	private Connection connect() {
+	        // SQLite connection string
+	        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+	        Connection conn = null;
+	        try {
+	            conn = DriverManager.getConnection(url);
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
+	        return conn;
+	 	}
+	 	
+	 	/**
+	     * Insert a new row into the warehouses table
+	     *
+	     * @param name
+	     * @param capacity
+	     */
+	    public void insert(String name, double capacity) {
+	        String sql = "INSERT INTO contacts(user) VALUES(?,?)";
+
+	        try (Connection conn = this.connect();
+	        	PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	            pstmt.setString(1, name);
+	            pstmt.setDouble(2, capacity);
+	            pstmt.executeUpdate();
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
 	    }
 
-	    /**
-	     * @param args the command line arguments
-	     */
-	    public static void main(String[] args) {
-	        createNewDatabase("test.db");
-	    }
+
+	 
+	 /**
+	  * @param args the command line arguments
+	 */
+	 public static void main(String[] args) {
+		 createNewDatabase("test.db");
+		 createTables();
+		 insert();
+	 }
 }
 
--- src/system/Tt.java
+++ src/system/Tt.java
@@ -20,6 +20,9 @@ import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class Tt {
 
@@ -54,28 +57,38 @@ public class Tt {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(204, 204, 204));
+		frame.setBackground(SystemColor.activeCaption);
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(Tt.class.getResource("/images/ACCUEIL_FOND.png")));
 		frame.getContentPane().setForeground(new Color(255, 255, 255));
 		frame.setBounds(100, 100, 885, 579);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Enter");
+		JButton btnNewButton = new JButton("Enter pseudonym");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		btnNewButton.setForeground(new Color(0, 0, 0));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 			}
 		});
-		btnNewButton.setBounds(557, 446, 169, 61);
+		btnNewButton.setBounds(473, 446, 169, 61);
 		frame.getContentPane().add(btnNewButton);
 		
-		textField = new JTextField();
-		textField.setBounds(90, 461, 400, 31);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
 		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBackground(new Color(204, 204, 204));
 		lblNewLabel.setIcon(new ImageIcon(Tt.class.getResource("/images/Fond.png")));
 		lblNewLabel.setBounds(162, 53, 504, 346);
 		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		textField.setBounds(176, 458, 270, 37);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 	}
 }
