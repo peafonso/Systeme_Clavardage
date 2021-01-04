b5a0a32b1c24d33a7de1817cf38bbc64ec9b027a
soumare <soumare@etud.insa-toulouse.fr>
2021-01-04 08:32:41 +0100

    Suite

--- src/Interface/Home.java
+++ src/Interface/Home.java
@@ -72,12 +72,13 @@ public class Home {
 		frame.setBackground(new Color(240, 240, 240));
 		//frame.setBounds(100, 100, 1640, 920);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1600,900);
+		//frame.setSize(1600,900);
 		ImageIcon homePicture = new ImageIcon();
 		homePicture=createImageIcon("/images/ACCUEIL_FOND2.jpg");
 		
     	//centrer la fenetre au milieu de l'ecran
     	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    	frame.setSize(dim);
         frame.setLocation(dim.width/2 - frame.getWidth()/2, dim.height/2 - frame.getHeight()/2);
         
         JMenuBar menuBar = new JMenuBar();
