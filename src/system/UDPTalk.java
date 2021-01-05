676fc0f0f3a2c5ae0fdccb1a0671de29bc28d400
peafonso <pafonso@etud.insa-toulouse.fr>
2021-01-05 14:51:34 +0100

    résolution broadcast

--- src/system/UDPTalk.java
+++ src/system/UDPTalk.java
@@ -1,11 +1,16 @@
 package system;
 
 import java.io.IOException;
+
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+import java.net.NetworkInterface;
 
 public class UDPTalk {
 
@@ -14,14 +19,36 @@ public class UDPTalk {
 	}
 	
     public static void broadcast(String broadcastMessage, int port) throws IOException {
-    	DatagramSocket socket = new DatagramSocket();
-        socket.setBroadcast(true);
+		for (InetAddress  addrbroadcast : listAllBroadcastAddresses()) {
+    		DatagramSocket socket = new DatagramSocket();
+    		socket.setBroadcast(true);
+    		byte[] buffer = broadcastMessage.getBytes();
+    		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, addrbroadcast, port);
+    		System.out.println("Envoi msg en broadcast");
+    		socket.send(packet);
+    		socket.close();
+		}
+    }
+    
+    
+    //liste des adresses de broadcast
+    static List<InetAddress> listAllBroadcastAddresses() throws SocketException {
+        List<InetAddress> broadcastList = new ArrayList<>();
+        Enumeration<NetworkInterface> interfaces 
+          = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = interfaces.nextElement();
 
-        byte[] buffer = broadcastMessage.getBytes();
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName("255.255.255.255"), port);
-		System.out.println("Envoi msg en broadcast");
-        socket.send(packet);
-        socket.close();
+            if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+                continue;
+            }
+
+            networkInterface.getInterfaceAddresses().stream() 
+              .map(a -> a.getBroadcast())
+              .filter(Objects::nonNull)
+              .forEach(broadcastList::add);
+        }
+        return broadcastList;
     }
     
     public static void sendUDP(String msg, int port, String laddr) throws SocketException {
