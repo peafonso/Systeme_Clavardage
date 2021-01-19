f31704e412b278bd1fe455fd60b4172ee1ce29fe
soumare <soumare@etud.insa-toulouse.fr>
2021-01-19 11:26:45 +0100

    Interface

---
+++ src/historique/Chat_with.java
@@ -0,0 +1,24 @@
+package historique;
+
+import model.User;
+
+public class Conversations {
+	
+	User friend;
+	//remplacer plus tard par un fichier contenant toute la conversation avec une personne 
+	//(faire un répertoire historique contenant les fichiers de conversation par ordre alphabétique de contact)
+	//test pour afficher message
+	//si nom du fichier n'existe pas, pas encore de conv avec la personne donc création du message
+	//si changement de pseudo, renommer le nom du fichier
+	String recu;
+
+	public Conversations() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void Chat_with() {
+		// TODO 
+	}
+	
+
+}
